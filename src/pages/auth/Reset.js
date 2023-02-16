74e806ab1434f96bd311799bb83bf0a89b663fbd
zoecabrales <zoe@old.st>
2023-02-16 23:15:26 +0800

    implemented reset password functionality using firebase

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,35 +1,82 @@
-import React from "react";
-import styles from "./auth.module.scss";
+// hooks
+import { useState } from "react";
+
+// asset
 import forgotImg from "../../assets/forgot.png";
+
+// navigation
 import { Link } from "react-router-dom";
+
+// components
 import Card from "../../components/card/Card";
 
+// notifs
+import { toast } from "react-toastify";
+
+// firebase utils
+import { auth } from "../../firebase/config";
+import { sendPasswordResetEmail } from "firebase/auth";
+
+// css
+import styles from "./auth.module.scss";
+import Loader from "../../components/loader/Loader";
+
 const Reset = () => {
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setIsLoading(false);
+        toast.success("Check your email for reset link");
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
+
   return (
-    <section className={`container ${styles.auth}`}>
-      <div className={styles.img}>
-        <img src={forgotImg} alt="Reset" width="400px" />
-      </div>
-      <Card>
-        <div className={styles.form}>
-          <h2>Reset Password</h2>
-          <form>
-            <input type="text" placeholder="Email" required />
-            <button className="--btn --btn-primary --btn-block">
-              Reset Password
-            </button>
-            <div className={styles.links}>
-              <p>
-                <Link to="/login">Login</Link>
-              </p>
-              <p>
-                <Link to="/register">Sign Up</Link>
-              </p>
-            </div>
-          </form>
+    <>
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={forgotImg} alt="Reset" width="400px" />
         </div>
-      </Card>
-    </section>
+        <Card>
+          <div className={styles.form}>
+            <h2>Reset Password</h2>
+
+            <form onSubmit={resetPassword}>
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Reset Password
+              </button>
+              <div className={styles.links}>
+                <p>
+                  <Link to="/login">Login</Link>
+                </p>
+                <p>
+                  <Link to="/register">Sign Up</Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
