64db64ac73ba7e6c9074a5b7c1cffd21a15589bb
zoecabrales <zoe@old.st>
2023-02-24 00:34:14 +0800

    fetching data using snapshot from firebase

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+import { toast } from "react-toastify";
+
+import { db } from "../../../firebase/config";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+
+import styles from "./ViewProducts.module.scss";
 
 const ViewProducts = () => {
-  return <div>ViewProducts</div>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, "products");
+
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  return <div></div>;
 };
 
 export default ViewProducts;
