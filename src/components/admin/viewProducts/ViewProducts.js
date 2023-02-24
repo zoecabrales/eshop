7506a35c378f75332723b38bdcf03672e83cd404
zoecabrales <zoe@old.st>
2023-02-24 23:56:42 +0800

    rendering products in the UI

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -2,10 +2,26 @@ import { useEffect, useState } from "react";
 
 import { toast } from "react-toastify";
 
-import { db } from "../../../firebase/config";
-import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import { db, storage } from "../../../firebase/config";
+import {
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
+import { ref } from "firebase/storage";
+
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+
+import Loader from "../../loader/Loader";
+
+import { Link } from "react-router-dom";
 import styles from "./ViewProducts.module.scss";
+import { deleteObject } from "firebase/storage";
+import { async } from "@firebase/util";
 
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
@@ -31,6 +47,7 @@ const ViewProducts = () => {
         }));
         console.log(allProducts);
         setProducts(allProducts);
+        setIsLoading(false);
       });
     } catch (error) {
       setIsLoading(false);
@@ -38,7 +55,73 @@ const ViewProducts = () => {
     }
   };
 
-  return <div></div>;
+  const deleteProduct = async (id, imageUrl) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+
+      const storageRef = ref(storage, imageUrl);
+      await deleteObject(storageRef);
+
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.table}>
+        <h2>All Products</h2>
+        {products.length === 0 ? (
+          <p>No product found.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Serial #</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => {
+                const { id, name, price, imageUrl, category, brand } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageUrl}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`$${price}`}</td>
+                    <td className={styles.icons}>
+                      <Link to="/admin/add-product">
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <FaTrashAlt
+                        size={20}
+                        color="red"
+                        onClick={() => deleteProduct(id, imageUrl)}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ViewProducts;
